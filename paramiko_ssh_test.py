153328c9ac4001b44105f0da942e40a85c1bf612
Ashwin Balakrishnan <nadarashwin@gmail.com>
2016-11-08 20:51:40 +0530

    # Please include a useful commit message!

---
+++ paramiko_ssh_test.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python2.7
+import paramiko
+
+def ssh_command(ip, user, passwd, command):
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(ip, username=user, password=passwd)
+	ssh_session = client.get_transport().open_session()
+	if ssh_session.active:
+		ssh_session.exec_command(command)
+		print ssh_session.recv(1024)
+		print ssh_session.recv_exit_status()
+	return
+
+ssh_command('0.0.0.0','root','xxxx','id')
