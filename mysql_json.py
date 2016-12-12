8326dfc5a3d0e765f0d21e77c8a790c2b4eb14ea
Ashwin Balakrishnan <nadarashwin@gmail.com>
2016-12-12 16:45:14 +0530

    updated the script for the dictionaries to be ordered

--- mysql_json.py
+++ mysql_json.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python2.7
 # encoding=utf8
+from collections import OrderedDict
 import MySQLdb
 import json
 
@@ -23,11 +24,11 @@ dic = {}
 for i in range(len(lst)):
  for j in first_result:
   if lst[i] in j:
-   dic[lst[i]] = {"name": j[0], "publisher": j[1], "description": j[2], "isPublic": 'true' if ord(j[3])==1 else "false","imageFile": j[5]}
+   dic[lst[i]] = OrderedDict ([ ("name", j[0]), ("publisher", j[1]), ("description", j[2]), ("isPublic", 'true' if ord(j[3])==1 else "false"), ("imageFile",  j[5]) ])
  for k in second_result:
   if lst[i] in k:
-   dic[lst[i]] = {"name": k[0], "publisher": k[1], "description": k[2], "isPublic": 'true' if ord(j[3])==1 else "false","imageFile": "_default.jpg"}
+   dic[lst[i]] = OrderedDict ([ ("name", k[0]), ("publisher", k[1]), ("description", k[2]), ("isPublic", 'true' if ord(k[3])==1 else "false"), ("imageFile", "informatica_default.jpg") ])
 
 with open('data.json', 'w') as outfile:
-    json.dump(dic, outfile, indent=4, sort_keys=True, separators=(',', ':'),encoding="ISO-8859-1")
+    json.dump(dic, outfile, indent=4, separators=(',', ':'),encoding="ISO-8859-1")
     print "output has been written to data.json" 
